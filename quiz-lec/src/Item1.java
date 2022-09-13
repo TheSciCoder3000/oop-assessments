6a5ec0f4ea93f256d49fa7825ed3a304a58269a4
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-09-13 13:23:44 +0800

    while loop commit

--- quiz-lec/src/Item1.java
+++ quiz-lec/src/Item1.java
@@ -5,27 +5,35 @@ public class Item1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		double even_sum = 0, odd_sum = 0, num_even = 0, num_odd = 0, num_input;
+		boolean again = true;
+		char again_res;
 		
-		System.out.print("Enter any number: ");
-		num_input = input.nextDouble();
-		
-		for (int x = 1; x <= num_input; x++) {
-			if (x % 2 != 0) {
-				System.out.println("The Odd number is: " + x);
-				odd_sum += x;
-				num_odd++;
+		while (again) {
+			System.out.print("Enter any number: ");
+			num_input = input.nextDouble();
+			
+			for (int x = 1; x <= num_input; x++) {
+				if (x % 2 != 0) {
+					System.out.println("The Odd number is: " + x);
+					odd_sum += x;
+					num_odd++;
+				}
 			}
-		}
-		System.out.println("The Average is: " + (odd_sum/num_odd));
-		
-		for (int x = 1; x <= num_input; x++) {
-			if (x % 2 == 0) {
-				System.out.println("The Even number is: " + x);
-				even_sum += x;
-				num_even++;
+			System.out.println("The Average is: " + (odd_sum/num_odd));
+			
+			for (int x = 1; x <= num_input; x++) {
+				if (x % 2 == 0) {
+					System.out.println("The Even number is: " + x);
+					even_sum += x;
+					num_even++;
+				}
 			}
+			System.out.println("The Average is: " + (even_sum/num_even));
+			System.out.print("Try Again [Yy/Nn]: ");
+			input.nextLine();
+			again_res = input.nextLine().charAt(0);
+			again = again_res == 'N' || again_res == 'n' ? false : true;
 		}
-		System.out.println("The Average is: " + (even_sum/num_even));
 	}
 
 }
